7f5c504f99099fd4b56dc98fdf89a141a590d128
Per Kristian <pekaaw@gmail.com>
2019-08-07 11:46:21 +0200

    Css loader update
    MiniCssExtractPlugin now supports HMR, as of April 10, 2019.
    Their loader can be used both in production and development.

--- webpack.config.js
+++ webpack.config.js
@@ -35,11 +35,7 @@ module.exports = {
 			{
 				test: /\.css$/,
 				use: [
-					/**
-					 * MiniCssExtractPlugin doesn't support HMR.
-					 * For developing, use 'style-loader' instead.
-					 * */
-					prod ? MiniCssExtractPlugin.loader : 'style-loader',
+					MiniCssExtractPlugin.loader,
 					'css-loader'
 				]
 			}
